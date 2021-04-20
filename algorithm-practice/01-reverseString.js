90b31233d45e5290eb72f43e924901d37e0e38c2
Sergei Liddell <liddell.s@pm.me>
2021-04-19 19:44:01 -0600

    Coverts to arrow function

--- algorithm-practice/01-reverseString.js
+++ algorithm-practice/01-reverseString.js
@@ -2,7 +2,7 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-reverseString = function(s) {
+const reverseString = s => {
   let last = s.length-1;
   for (let first = 0; first < last; first ++) {
       let temp = s[first];
