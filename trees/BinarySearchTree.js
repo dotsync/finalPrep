a5fb6b8c72e1594eacd29c2704244f5ae7bfbf39
Sergei Liddell <liddell.s@pm.me>
2021-04-20 18:48:50 -0600

    Creates BSTNode class

--- trees/BinarySearchTree.js
+++ trees/BinarySearchTree.js
@@ -1,3 +1,11 @@
+class BSTNode {
+  constructor(val) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
 class BinarySearchTree {
   constructor() {
     this.root = null;
