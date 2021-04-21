fb0c5253c8debbd03d31497d63fb7b5442c1b4ad
Sergei Liddell <liddell.s@pm.me>
2021-04-20 18:59:37 -0600

    Creates test suite for BST node class

--- trees/BinarySearchTree.test.js
+++ trees/BinarySearchTree.test.js
@@ -1,8 +1,19 @@
 const { BSTNode, BinarySearchTree } = require('./BinarySearchTree')
 
-describe('Tree classes test Suite', () => {
-  test('should retun a node', () => {
-    const bstNode = new BSTNode('data');
-    console.log(bstNode)
+describe('Binary Search Tree', () => {
+  describe('Node Class', () => {
+    const bstNode = new BSTNode('Hello world!')
+    test('should have a left property', () => {
+      expect(bstNode.left).toBeNull();
+    })
+    test('should have a right property', () => {
+      expect(bstNode.right).toBeNull();
+    })
+    test('should store data', () => {
+      expect(bstNode.val).toBe('Hello world!');
+    })
+  })
+  describe('Tree class', () => {
+
   })
 })
