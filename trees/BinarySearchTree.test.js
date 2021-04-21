552c0b91d71888e934728cd378bcfdebee1f2b53
Sergei Liddell <liddell.s@pm.me>
2021-04-20 19:40:10 -0600

    Cretaes test suite for empty BST

--- trees/BinarySearchTree.test.js
+++ trees/BinarySearchTree.test.js
@@ -1,7 +1,7 @@
 const { BSTNode, BinarySearchTree } = require('./BinarySearchTree')
 
 describe('Binary Search Tree', () => {
-  describe('Node Class', () => {
+  describe('BST Node Class', () => {
     const bstNode = new BSTNode('Hello world!')
     test('should have a left property', () => {
       expect(bstNode.left).toBeNull();
@@ -13,7 +13,19 @@ describe('Binary Search Tree', () => {
       expect(bstNode.val).toBe('Hello world!');
     })
   })
-  describe('Tree class', () => {
+  describe('BST Tree class', () => {
+    test('should instantiate an empty tree', () => {
+      const bst = new BinarySearchTree;
+      expect(bst.root).toBeNull();
+    })
 
+    describe('insert', () => {
+      test('should insert into an empty Tree', () => {
+        const bst = new BinarySearchTree;
+      })
+      test('should insert new nodes in the correct position', () => {
+
+      })
+    })
   })
 })
